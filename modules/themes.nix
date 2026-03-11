75ac6535c822d7144a34bf0e85ed4ca1c8115d9c
Pi Lanningham <pi.lanningham@gmail.com>
2026-03-11 01:57:26 -0400

    Add 10 new themes with matching wallpapers
    New themes: catppuccin-mocha, rose-pine, rose-pine-moon, dracula,
    oxocarbon-dark, tokyo-night-storm, ayu-dark, horizon-dark,
    material-palenight, solarized-dark. Each has a curated wallpaper
    (shared where themes are variants of the same family).
    
    Total theme count: 17

--- modules/themes.nix
+++ modules/themes.nix
@@ -36,4 +36,54 @@
     vscode-theme = "Gruvbox Light Medium";
     wallpaper = "gruvbox-1.jpg";
   };
+  "catppuccin-mocha" = {
+    base16-theme = "catppuccin-mocha";
+    vscode-theme = "Catppuccin Mocha";
+    wallpaper = "catppuccin-mocha-1.jpg";
+  };
+  "rose-pine" = {
+    base16-theme = "rose-pine";
+    vscode-theme = "Rosé Pine";
+    wallpaper = "rose-pine-1.jpg";
+  };
+  "rose-pine-moon" = {
+    base16-theme = "rose-pine-moon";
+    vscode-theme = "Rosé Pine Moon";
+    wallpaper = "rose-pine-1.jpg";
+  };
+  "dracula" = {
+    base16-theme = "dracula";
+    vscode-theme = "Dracula";
+    wallpaper = "dracula-1.jpg";
+  };
+  "oxocarbon-dark" = {
+    base16-theme = "oxocarbon-dark";
+    vscode-theme = "Oxocarbon Dark";
+    wallpaper = "oxocarbon-1.jpg";
+  };
+  "tokyo-night-storm" = {
+    base16-theme = "tokyo-night-storm";
+    vscode-theme = "Tokyo Night Storm";
+    wallpaper = "1-Pawel-Czerwinski-Abstract-Purple-Blue.jpg";
+  };
+  "ayu-dark" = {
+    base16-theme = "ayu-dark";
+    vscode-theme = "Ayu Dark";
+    wallpaper = "ayu-dark-1.jpg";
+  };
+  "horizon-dark" = {
+    base16-theme = "horizon-dark";
+    vscode-theme = "Horizon";
+    wallpaper = "horizon-1.jpg";
+  };
+  "material-palenight" = {
+    base16-theme = "material-palenight";
+    vscode-theme = "Material Palenight";
+    wallpaper = "palenight-1.jpg";
+  };
+  "solarized-dark" = {
+    base16-theme = "solarized-dark";
+    vscode-theme = "Solarized Dark";
+    wallpaper = "solarized-1.jpg";
+  };
 }
